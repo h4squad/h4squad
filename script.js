978702949d8be6036cb6896638ebcbeadd05858a
h4squad <rao141937@gmail.com>
2025-08-03 01:14:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ function showPage(id) {
     section.classList.add('active');
   }
 }
+
 window.onload = () => {
   showPage('home');
+
+  // Adsterra native banner ad injection
+  const adDiv = document.createElement('div');
+  adDiv.id = 'adsterra-banner';
+  adDiv.style = 'margin: 20px auto; text-align: center;';
+  document.body.appendChild(adDiv);
+
+  const atOptions = {
+    'key': '27228034',
+    'format': 'iframe',
+    'height': 50,
+    'width': 320,
+    'params': {}
+  };
+
+  const adScript = document.createElement('script');
+  adScript.type = 'text/javascript';
+  adScript.src = `https://www.profitabledisplaynetwork.com/${atOptions.key}/invoke.js`;
+  adDiv.appendChild(adScript);
 };
